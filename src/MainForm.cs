3fa2006617afb71c1fe335eb318b9383a30ba712
Gabriel dos Prazeres <gabrieldosprazeres@hotmail.com.br>
2025-07-07 10:02:51 -0300

    Initial commit

---
+++ src/MainForm.cs
@@ -0,0 +1,14 @@
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace ROAutomationToolkit
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            this.Icon = new Icon("resources\\icon.ico");
+        }
+    }
+}
